dcaac982ab3ccce7ebd1aa9305474b5b9ba905d0
j2ho <92702929+j2ho@users.noreply.github.com>
2025-08-20 16:39:55 +0900

    remove function to utils

--- Site2/site.py
+++ Site2/site.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# modified 2024 by jiho 
 
 import sys
 import os
@@ -7,9 +6,8 @@ import copy
 import Galaxy
 from Galaxy.utils.libtm import TM_result
 from .libfr_site import SiteTemplate, search_site_template
-import functools
+from .utils import calculate_similarity
 import subprocess
-import numpy as np
 import time 
 
 # TM cutoff for template selection
@@ -19,16 +17,15 @@ TM_CUT = 0.2
 type_dict = {1:'lipid',2:'non-biological',3:'ions/metal',4:'glycan',0:'general'}
 
 # OpenBabel path
-BABEL = '/applic/OpenBabel/current/bin/obabel' # 2.4.1
-BABEL_3 = '/applic/OpenBabel/3.1.1/bin/obabel' # 3.1.1
+BABEL = '/your/path/to/obabel' 
 
 # default FP2
 finger_print = ['-xfFP3', '-xfFP4', '-xfMACCS']
 
+
 class Ligand:
     def __init__(self, ligName):
         self.is_valid = True
-        #self.lig_name = ligName
         self.lig_name = ligName.split('_')[0]
         self.lig_type = type_dict[int(ligName.split('_')[1])]
         self.templ_s = []
@@ -41,14 +38,13 @@ class Ligand:
         self.templ_s.append(templ)
         self.n_lig += 0
         
-    def get_score(self, query_lig_fn=None, fptype=None, simtype=None):
+    def get_score(self, query_lig_fn=None, fptype=None):
         self.templ_score_s = [] 
         for templ in self.templ_s:
             if not templ.is_valid: continue
             templ_score = templ.score
             if query_lig_fn is not None: 
-                similarity = calculate_similarity(self.lig_name, query_lig_fn) 
-                #templ_score_v2 = templ_score*similarity
+                similarity = calculate_similarity(self.lig_name, query_lig_fn, fptype=fptype) 
                 templ_score_v2 = similarity # just ligand similarity
             elif query_lig_fn is None: 
                 templ_score_v2 = -1.0
@@ -72,60 +68,40 @@ class Ligand:
             templ.write(templ_home)
             tm = Galaxy.utils.TM_align(templ.pdb_fn, pdb.pdb_fn)
             templ.tm = tm
-            templ.rewrite(templ_home,self.lig_name) # write lig_name
-        #
+            templ.rewrite(templ_home,self.lig_name) 
         self.templ_s.append(templ)
-
-
-def calculate_similarity(temp_lig_name, query_lig, fptype='FP2', simtype='tani'):
-    sdf_file = f'/home/j2ho/DB/site_sdfs/{temp_lig_name}.sdf'
-    p1=sdf_file
-    p2=query_lig
-    if fptype == 'FP2':
-        cmd = '%s %s %s -ofpt 2> /dev/null'%(BABEL_3,p1,p2)
-    else:
-        cmd = '%s %s %s -ofpt -%s 2> /dev/null'%(BABEL_3,p1,p2,fptype)
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-    g = proc.stdout.readlines()
-    for ln in g:
-        ln = ln.decode()
-        if 'Tanimoto' in ln:
-            tan=ln.split()[-1]
-    return float(tan)
 
 
 def select_ligand(job, pdb, re_run=False, **kwargs):
     out_f_s = {'Site.selected_lig.dat': Galaxy.core.FilePath('%s.selected_lig.dat'%job.title)}
-    #
+
     if 'benchmark' in kwargs:
         benchmark = kwargs['benchmark']
-    #
+
     if (not re_run) and Galaxy.core.file_status(out_f_s):
         job.mkdir('templ_s', tag='SITE_templ_HOME', cd=False)
         job.update(out_f_s)
         sys.stdout.write("INFO: Prediction of bound ligand is already done, and this result will be re-used.\n")
         ligand_s = read_selected_lig(out_f_s['Site.selected_lig.dat'], pdb, job['SITE_templ_HOME'])
         return ligand_s
-    #
-    #
+
     if 'search_method' in kwargs:
         search_method = kwargs['search_method'] 
     else:
         search_method = 'foldseek'
     lig_fn = kwargs['lig_fn']
     fptype = kwargs['fptype']
-    simtype = kwargs['simtype']
-    #
+
     templ_s = search_site_template(job, pdb.pdb_fn, job.fa_fn, search_method=search_method,
                                    re_run=re_run, benchmark=benchmark)
-    #
+
     if len(templ_s) == 0:
         sys.stdout.write('INFO: There is no template to detect binding site. Terminate.\n')
         fout = open('%s.selected_lig.dat'%job.title, 'wt')
         fout.write('')
         fout.close()
         return []
-    #
+    
     job.mkdir('templ_s', tag='SITE_templ_HOME', cd=False)
     for i in range(len(templ_s)):
         best_templ = templ_s[0]
@@ -133,23 +109,19 @@ def select_ligand(job, pdb, re_run=False, **kwargs):
         if best_templ.is_valid:
             templ_s = templ_s[i:]
             break
-    #
+
     if not (best_templ.is_valid):
         sys.stdout.write('INFO: There is no template to detect binding site. Terminate.\n')
         fout = open('%s.selected_lig.dat'%job.title, 'wt')
         fout.write('')
         fout.close()
         return []
-    #
-    tm_cutoff = TM_CUT
-    #
     # Check global structure similarity to query protein
     ligand_s = []
     ligName_list = []
-    #
+
     n_valid = 0
     t1 = time.time()
-    totaltm = 0
     for templ in templ_s:
         templ.write(job['SITE_templ_HOME'])
         if not templ.is_valid:
@@ -188,28 +160,23 @@ def select_ligand(job, pdb, re_run=False, **kwargs):
     query_lig = lig_fn
     if not query_lig == None: 
         if not len(query_lig.split('.')) > 1:
-            sdf_file = f'/home/j2ho/DB/site_sdfs/{query_lig}.sdf'
+            sdf_file = f'/path/to/ligand/sdf/db/{query_lig}.sdf'
             if not os.path.exists(sdf_file):
                 sdf_link = f'https://files.rcsb.org/ligands/download/{query_lig}_ideal.sdf'
-                os.system(f'wget {sdf_link} -O /home/j2ho/DB/site_sdfs/{query_lig}.sdf') 
+                os.system(f'wget {sdf_link} -O /path/to/ligand/sdf/db/{query_lig}.sdf') 
             query_lig = sdf_file
     for ligand in ligand_s:
-        sdf_file = f'/home/j2ho/DB/site_sdfs/{ligand.lig_name}.sdf'
+        sdf_file = f'/path/to/ligand/sdf/db/{ligand.lig_name}.sdf'
         if not os.path.exists(sdf_file): 
             sdf_link = f'https://files.rcsb.org/ligands/download/{ligand.lig_name}_ideal.sdf'
-            os.system(f'wget {sdf_link} -O /home/j2ho/DB/site_sdfs/{ligand.lig_name}.sdf') 
-        ligand.get_score(query_lig_fn=query_lig, fptype=fptype, simtype=simtype)
-    #
+            os.system(f'wget {sdf_link} -O /path/to/ligand/sdf/db/{ligand.lig_name}.sdf') 
+        ligand.get_score(query_lig_fn=query_lig, fptype=fptype)
+
     new_ligand_s = [] 
     wrt_lig = []
     wrt_nonlig = []
     wrt_metal = []
-    i_lig = 0
-    #
-    #
-    from Galaxy.core.vector import distance
     t3 = time.time()
-    final_templ_s = {}
     for ligand in ligand_s:
         lig_name = ligand.lig_name
         lig_type = ligand.lig_type
@@ -222,14 +189,12 @@ def select_ligand(job, pdb, re_run=False, **kwargs):
             if not templ.is_valid:
                 ligand.is_valid = templ.is_valid
                 continue
-            templ_name = f'{templ.pdb_id}_{templ.chain_id}'
             for elem in templ.contact_ligs: 
                 lig_name = elem[1]
-                templ.rewrite(job['SITE_templ_HOME'],lig_name) # write lig_name
+                templ.rewrite(job['SITE_templ_HOME'],lig_name) 
                 ligcntr = templ.get_lig_cntr(job['SITE_templ_HOME'],lig_name)
                 tmp.append([templ, score, score_lig])
                 new_ligand_s.append([lig_name.strip(), lig_type, templ, score, score_lig, ligcntr])
-    #
     t4 = time.time()
     print (f'check templ validity, rewrite aligned pdb: {t4-t3}sec',flush=True)
     
@@ -266,18 +231,15 @@ def select_ligand(job, pdb, re_run=False, **kwargs):
     fout = open('%s.selected_lig.dat'%job.title, 'wt')
     fout.writelines(wrt_lig)
     fout.close()
-    #
     fout = open('%s.selected_nonbio.dat'%job.title, 'wt')
     fout.writelines(wrt_nonlig)
     fout.close()
-    #
     fout = open('%s.selected_metal.dat'%job.title, 'wt')
     fout.writelines(wrt_metal)
     fout.close()
-    #
     os.system('rm *.pdb')
     job.update(out_f_s)
-    #
+
     return ligand_s
 
 
@@ -297,15 +259,13 @@ def read_selected_lig(selected_fn, pdb, templ_home):
                 ligand.read_templ(pdb, line, templ_home)
     return ligand_s
 
+
 def extract_rsr(job, pdb, ligand, re_run=False):
     templ_fn_s = []
     tm_s = []
     for templ in ligand.templ_s:
-#        templ_fn_s.append(templ.pdb_fn_lig[ligand.lig_name])
         templ_fn_s.append(templ.pdb_fn)
         tm_s.append(templ.tm.tm)
-    #
     rsr_fn = Galaxy.galaxy.site_restraint(job, pdb, ligand.lig_fn, templ_fn_s, tm_s,\
                 outfile_prefix='%s_rsr'%ligand.lig_name, lig_name=ligand.lig_name, re_run=re_run)
-    #
     return rsr_fn
--- Site2/utils.py
+++ Site2/utils.py
@@ -1,17 +1,18 @@
-import os
-import sys 
 import subprocess
 
-BABEL = '/applic/OpenBabel/current/bin/obabel' # 2.4.1
-BABEL_3 = '/applic/OpenBabel/3.1.1/bin/obabel' # 3.1.1
-# default FP2
+BABEL = '/your/path/to/obabel' 
+
 finger_print = ['-xfFP3', '-xfFP4', '-xfMACCS']
 
-def calculate_similarity(temp_lig_name, query_lig):
-    sdf_file = f'/home/j2ho/DB/site_sdfs/{temp_lig_name}.sdf'
+def calculate_similarity(temp_lig_name, query_lig, fptype='FP2'):
+    # ligand name = CCD code 
+    sdf_file = f'/path/to/ligand/sdf/db/{temp_lig_name}.sdf'
     p1=sdf_file
     p2=query_lig
-    cmd = '%s %s %s -ofpt 2> /dev/null'%(BABEL_3,p1,p2)
+    if fptype == 'FP2':
+        cmd = '%s %s %s -ofpt 2> /dev/null'%(BABEL,p1,p2)
+    else:
+        cmd = '%s %s %s -ofpt -%s 2> /dev/null'%(BABEL,p1,p2,fptype)
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     g = proc.stdout.readlines()
     for ln in g:
